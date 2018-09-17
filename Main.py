cba7ae18f4b584781e32f89db8b16c0850d7007e
EmileTrotignon <emile.trotignon@laposte.net>
2018-09-17 22:41:00 +0200

    Implement working Board.run_trains function Vanilla hexes needs to be added Surface tested

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@
 
 
 from copy import copy, deepcopy
-from typing import List, Union, Dict, Optional
+from typing import List, Union, Dict, Optional, Callable
 from graphlib.main import *
 import ascii_hex as ah
 
@@ -24,6 +24,14 @@ import ascii_hex as ah
 def rotate(li, n):
 
     return li[n:] + li[:n]
+
+
+def opposite_side(side: int) -> int:
+
+    if side < 3:
+        return side + 3
+    else:
+        return side - 3
 
 
 class Str2D:
@@ -55,7 +63,7 @@ class Str2D:
 
         return '\n'.join(''.join(li) for li in self)
 
-    def paint_over(self, other: 'Str2D', start=(0, 0), ignore=' ') -> 'Str2D':
+    def paint_over(self, other: 'Str2D', start=(0, 0), ignore=' ', ignore2='_/\\') -> 'Str2D':
 
         x0, y0 = start
 
@@ -63,7 +71,8 @@ class Str2D:
 
             for x, char in enumerate(line):
 
-                if y + y0 < len(self) and x0 + x < len(self[y0 + y]) and char != ignore:
+                if y + y0 < len(self) and x0 + x < len(self[y0 + y]) \
+                        and char not in ignore and not(char in ignore2 and self[y0 + y][x0 + x] not in ignore):
                     self[y0 + y][x0 + x] = char
 
         return self
@@ -76,137 +85,144 @@ class Locus:
 
 class Track(Locus):
 
-    def __init__(self, hexagon, end_list0=None, end_list1=None):
+    def __init__(self, end_list=None):
 
-        self.hexagon = hexagon
-        self.end1 = [] if end_list0 is None else end_list0
-        self.end2 = [] if end_list1 is None else end_list1
-        self.end_tuple = (self.end1, self.end2)
+        self.n = -1
+        self.end_list = [] if end_list is None else end_list
         self.value = 0
 
     def __repr__(self):
 
-        return 'Track object connected to {} and {} in {}'.format(str(self.end1), str(self.end2), str(self.hexagon))
+        return f'Track [|{str(self.end_list)}|]"'
 
-    def connect(self, number, other_track_end):
+    def connect(self, other_locus: Union['Track', 'City']):
+        """ One way connection"""
 
-        if number == 1:
-            self.end1.end_list.append(other_track_end)
-            other_track_end.end_list.append(self.end1)
-        if number == 2:
-            self.end2.end_list.append(other_track_end)
-            other_track_end.end_list.append(self.end2)
+        self.end_list.append(other_locus)
 
-    def neighbouring_locis(self, entry='start'):
+    def neighbouring_locii(self, entry='start'):
         """ Return the connected end objects, except for the entry"""
 
-        final_list = copy(self.end1 + self.end2)
-
         if entry != 'start':
+            final_list = copy(self.end_list)
             final_list.remove(entry)
+            return final_list
 
-        return final_list
+        return self.end_list
 
 
 class City(Locus):
 
-    def __init__(self, name, hexagon, value, token_list=None):
+    def __init__(self, value, name='no_name', token_list=None, max_token=1):
 
+        #self.n = -1
         self.name = name
-        self.hexagon = hexagon
         self.value = value
         self.token_list = [] if token_list is None else token_list
         self.end_list = []
+        self.max_token = max_token
+
+    def __repr__(self):
+
+        return f'city'
 
     def __str__(self):
 
-        rep = f'City named {self.name}, worth {self.value} in {str(self.hexagon)} with those tokens : {self.token_list}'
+        rep = f'City named {self.name}, worth {self.value} with those tokens : {self.token_list}'
         return rep
 
-    def connect(self, track: Track, end_number: int):
+    def connect(self, track: Track):
+
+        """ One way connection"""
 
         self.end_list.append(track)
-        track.end_tuple[end_number].append(self)
 
     def add_token(self, company):
 
-        self.token_list.append(company)
-        self.token_list.append(self)
+        if len(self.token_list) < self.max_token:
+            self.token_list.append(company)
+            company.token_list.append(self)
+        else:
+            print(f'city full, cant add token: max token = {self.max_token}, token placed = {len(self.token_list)}')
 
-    def neighbouring_locis(self, entry='start'):
+    def neighbouring_locii(self, entry='start'):
 
         """ Return the connected end objects, except for the entry"""
 
-        final_list = copy(self.end_list)
-
         if entry != 'start':
+            final_list = copy(self.end_list)
             final_list.remove(entry)
 
-        return final_list
+            return final_list
+
+        return self.end_list
+
+
+def connect(locus0: Union['Track', 'City'], locus1: Union['Track', 'City']):
+
+    locus0.connect(locus1)
+    locus1.connect(locus0)
 
 
 class Hexagon:
 
-    def __init__(self, track_list: List[Track], city_dic: Dict[str, City], side_list: List[Tuple[Track, int]],
+    def __init__(self, track_list: List[Track], city_list: List[City],
+                 side_tuple: Tuple[List[Track]]=None,
                  upgradeable=True, ascii_repr=ah.UNKNOWN_TILE):
 
         self.track_list = track_list
-        self.city_dic = city_dic
-        self.side_list = side_list
+        self.city_list = city_list
+        self.side_tuple = side_tuple if side_tuple is not None else ([], [], [], [], [], [])
         self.upgradeable = upgradeable
         self._ascii_repr = Str2D(ascii_repr[1:])
 
     def __repr__(self) -> str:
 
-        return f'Hexagon({self.track_list}, {self.city_dic}, upgradeable={self.upgradeable},' \
+        return f'Hexagon({self.track_list}, {self.city_list}, upgradeable={self.upgradeable},' \
                f' ascii_repr={self._ascii_repr}'
 
     def __str__(self) -> str:
 
         return str(self._ascii_repr)
 
-    def rotated(self, n_rot: int) -> 'Hexagon':
+    def __getitem__(self, key):
 
-        return Hexagon(self.track_list, self.city_dic, rotate(self.side_list, n_rot))
+        return self.side_tuple[key]
+
+    def rotated(self, n_rot: int) -> 'Hexagon':
+        # rotate clockwise
+        return Hexagon(self.track_list, self.city_list, rotate(self.side_tuple, n_rot))
 
     def str_2d(self) -> Str2D:
 
         return self._ascii_repr
 
-    def add_track(self, end1, end2):
+    def add_track(self, connected:List[Union[Track, City, int]]):
+        """add track to unplaced hexagon"""
 
-        self.track_list.append(Track(self, end1, end2))
+        track = Track()
+        for end in connected:
+            if type(end) == int:
+                self.side_tuple[end].append(track)
+            else:
+                connect(track, end)
 
-        for e1 in end1:
-            if type(e1) == City:
+        self.track_list.append(track)
 
-                e1.co_track_list.append(self)
+    def connect(self, other: 'Hexagon', side: int):
 
-            if type(e1) == Track:
+        self_track_list = self[side]
+        other_track_list = other[opposite_side(side)]
+        for self_track in self_track_list:
+            for other_track in other_track_list:
+                connect(self_track, other_track)
 
-                # so that end1 is always connected to another track's end2 and vice versa
-                end1.end2.append(self)
+    def add_city(self, value, token_list=None, name='no_name', max_token=1):
 
-        for e2 in end2:
-
-            if type(e2) == City:
-
-                end2.co_track_list.append(self)
-
-            if type(e2) == Track:
-
-                # so that end1 is always connected to another track's end2 and vice versa
-                e2.e1.append(self)
-
-    def add_city(self, name, hexagon, value, token_list):
-
-        self.city_dic[name] = City(name, hexagon, value, token_list)
-
-    def rotate(self):
-        return self
+        self.city_list.append(City(value, token_list=token_list, name=name, max_token=max_token))
 
 
-empty_hex = Hexagon([], {}, ascii_repr=ah.EMPTY_TILE)
+empty_hex = Hexagon([], [], ascii_repr=ah.EMPTY_TILE)
 
 
 class Company:
@@ -247,6 +263,16 @@ class Board:
 
         return str(self.ascii_repr) if self.ascii_repr is not None else str(self.gen_ascii_repr())
 
+    def __getitem__(self, key: Tuple[int, int]):
+
+        return self.hex_dic[key]
+
+    def get_neighbours_coors(self, hex_coor: Tuple[int, int]) -> Tuple:
+
+        x = hex_coor[0]
+        y = hex_coor[1]
+        return (x, y - 2), (x + 1, y - 1), (x + 1, y + 1), (x, y + 2), (x - 1, y - 1), (x - 1, y + 1)
+
     def gen_hex_dic(self):
 
         """Generate a rectangular hex board"""
@@ -266,22 +292,32 @@ class Board:
         hx = len(empty_hex.str_2d()[0])
         line = ' ' * (15 * self.resolution[0] + 4)
         self.ascii_repr = Str2D(((line + '\n') * int((((hy - 1) * self.resolution[1] + hy + 1) / 2)))[:-1])
-        print(hy)
 
         for x, y in self.hex_dic:
 
-            self.ascii_repr.paint_over(empty_hex.str_2d(), start=(x * hx, y * (hy - 1) // 2))
+            self.ascii_repr.paint_over(self.hex_dic[x, y].str_2d(), start=(x * hx, y * (hy - 1) // 2))
 
         return self.ascii_repr
 
-    def upgrade_hex(self, x, y, new_hex, check_rules=True):
+    def upgrade_hex(self, x: int, y: int, new_hex: Callable[[], Hexagon], check_rules=True):
 
         old_hex = self.hex_dic[(x, y)]
         if (not check_rules) or old_hex.upgradeable:
-            self.hex_dic[(x, y)] = new_hex
+
+            self.hex_dic[(x, y)] = new_hex()
+
+            for side, neighbours_coors in enumerate(self.get_neighbours_coors((x, y))):
+
+                try:
+
+                    neighbour = self.hex_dic[neighbours_coors]
+                    self.hex_dic[(x, y)].connect(neighbour, side)
+                except KeyError:
+                    pass
+
+            self.gen_ascii_repr()
             return True
         return False
-
 
     def add_company(self, company: Company):
 
@@ -290,24 +326,25 @@ class Board:
         self.company_dic[company.name] = company
 
     def generate_graph(self, company=None) -> WeightedVerticesGraph:
-        locis_list = sum((hexa.track_list + hexa.city_list for hexa in self.hex_dic), [])
+        locii_list = sum((self.hex_dic[coor].track_list + self.hex_dic[coor].city_list for coor in self.hex_dic), [])
 
         i = 0
-        for locus in locis_list:
+        for locus in locii_list:
             locus.n = i
             i += 1
 
-        neighbouring_locis_list = [locus.neighbouring_locis() for locus in locis_list]
+        neighbouring_locii_list = [locus.neighbouring_locii() for locus in locii_list]
 
         if company is not None:  # Check tokens
 
-            for k, locus in enumerate(locis_list):
+            """for k, locus in enumerate(locii_list):
 
-                if type(locus) == City and None not in locus.token_list and company not in locus.token_list:
-                    neighbouring_locis_list[k] = []
+                if type(locus) == City and len(locus.token_list) >= locus.max_token and company not in locus.token_list:
+                    neighbouring_locii_list[k] = []"""
 
-        neighbour_list = [[locus.n for locus in neighbour_list] for neighbour_list in neighbouring_locis_list]
-        weight_list = [locus.value for locus in locis_list]
+        neighbour_list = [list(set([locus.n for locus in neighbour_list])) for neighbour_list in neighbouring_locii_list]
+        weight_list = [locus.value for locus in locii_list]
+        #g = neighbour_list[1000000000000]
         graph = WeightedVerticesGraph(neighbour_list, weight_list)
 
         return graph
@@ -318,5 +355,3 @@ def hex_to_char_coord(x, y):
     hy = len(empty_hex.str_2d())
     hx = len(empty_hex.str_2d()[0])
     return x * hx, y * (hy - 1) // 2
-
-print(Board({}, (20, 20)))
--- vanilla_hex.py
+++ vanilla_hex.py
@@ -1,22 +1,62 @@
 from Main import *
 import ascii_hex as ah
 
-empty = Hexagon([], {}, ascii_repr=ah.EMPTY_TILE)
+empty = Hexagon([], [], ascii_repr=ah.EMPTY_TILE)
 
-A1 = Hexagon([], {}, ascii_repr=ah.A1)
-A2 = Hexagon([], {}, ascii_repr=ah.A2)
-A3 = Hexagon([], {}, ascii_repr=ah.A3)
-A4 = Hexagon([], {}, ascii_repr=ah.A4)
-A5 = Hexagon([], {}, ascii_repr=ah.A5)
-A6 = Hexagon([], {}, ascii_repr=ah.A6)
-A7 = Hexagon([], {}, ascii_repr=ah.A7)
-A8 = Hexagon([], {}, ascii_repr=ah.A8)
-A9 = Hexagon([], {}, ascii_repr=ah.A9)
-A10 = Hexagon([], {}, ascii_repr=ah.A10)
-A11 = Hexagon([], {}, ascii_repr=ah.A11)
-A12 = Hexagon([], {}, ascii_repr=ah.A12)
-A13 = Hexagon([], {}, ascii_repr=ah.A13)
-A14 = Hexagon([], {}, ascii_repr=ah.A14)
+"""A1 = Hexagon([], [], ascii_repr=ah.A1)
+A1.add_track(0, 3)
 
-bb1 = Hexagon([], {}, ascii_repr=ah.bb1)
-bb5 = Hexagon([], {}, ascii_repr=ah.bb5)
+A2 = Hexagon([], [], ascii_repr=ah.A2)
+A2.add_track(3, 5)
+
+A3 = Hexagon([], [], ascii_repr=ah.A3)
+A3.add_track(3, 4)
+
+A4 = Hexagon([], [], ascii_repr=ah.A4)
+A4.add_city(10, max_token=0)
+A4.add_track(0, [A4.city_list[0]])
+A4.add_track([A4.city_list[0]], 3)
+
+A5 = Hexagon([], [], ascii_repr=ah.A5)
+A5.add_city(10, max_token=0)
+A5.add_track(5, [A5.city_list[0]])
+A5.add_track([A5.city_list[0]], 3)
+
+A6 = Hexagon([], [], ascii_repr=ah.A6)
+A6.add_city(10, max_token=0)
+A6.add_track(4, [A6.city_list[0]])
+A6.add_track([A6.city_list[0]], 3)"""
+
+
+def a7():
+
+    nh = Hexagon([], [], ascii_repr=ah.A7)
+    nh.add_city(20, max_token=1)
+    nh.add_track([nh.city_list[0], 0])
+    nh.add_track([nh.city_list[0], 3])
+    return nh
+
+"""A8 = Hexagon([], [], ascii_repr=ah.A8)
+A9 = Hexagon([], [], ascii_repr=ah.A9)
+A10 = Hexagon([], [], ascii_repr=ah.A10)
+A11 = Hexagon([], [], ascii_repr=ah.A11)
+A12 = Hexagon([], [], ascii_repr=ah.A12)
+A13 = Hexagon([], [], ascii_repr=ah.A13)
+A14 = Hexagon([], [], ascii_repr=ah.A14)
+
+bb1 = Hexagon([], [], ascii_repr=ah.bb1)
+bb5 = Hexagon([], [], ascii_repr=ah.bb5)"""
+
+
+board = Board({}, (5, 5))
+board.upgrade_hex(0, 0, a7)
+board.upgrade_hex(0, 2, a7)
+board.upgrade_hex(0, 4, a7)
+company = Company('company', [], [3, 2], n_token=2)
+board[(0, 0)].city_list[0].add_token(company)
+board[(0, 4)].city_list[0].add_token(company)
+
+print(board)
+best_path = company.run_trains(board)
+print(sum(len(path) for path in best_path))
+
